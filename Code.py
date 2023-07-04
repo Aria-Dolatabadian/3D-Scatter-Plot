6594dcf33627712a6c51accd3c39df7855015bf3
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-07-04 18:52:26 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+#Read data
+df = pd.read_csv("yiled components.csv")
+#Scatter Plot
+fig = plt.figure(figsize=(10, 10))
+ax = plt.axes(projection="3d")
+ax.scatter3D(df['Plant number'], df['Seed number'], df['Seed weight'],
+             c = df['Cultivar'], s = df['Yield']/50, alpha = 0.4)
+ax.set_xlabel("Plant number")
+ax.set_ylabel("Seed number")
+ax.set_zlabel("Seed weight")
+ax.set_title("Investigating the relationship between seed weight, seed number, and plant number on seed yield across three different cultivars")
+plt.show()
+
