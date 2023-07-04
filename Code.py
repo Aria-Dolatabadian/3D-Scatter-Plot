9628bdf2ebc22d0e95fb16eed29b391f09a33881
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-07-04 18:57:22 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,16 +1,23 @@
 import pandas as pd
-
 import matplotlib.pyplot as plt
-#Read data
+
+# Read data
 df = pd.read_csv("yiled components.csv")
-#Scatter Plot
+
+# Scatter Plot
 fig = plt.figure(figsize=(10, 10))
 ax = plt.axes(projection="3d")
-ax.scatter3D(df['Plant number'], df['Seed number'], df['Seed weight'],
-             c = df['Cultivar'], s = df['Yield']/50, alpha = 0.4)
+scatter = ax.scatter3D(df['Plant number'], df['Seed number'], df['Seed weight'],
+                       c=df['Cultivar'], s=df['Yield']/50, alpha=0.4)
+
+# Create a legend
+legend_labels = df['Cultivar'].unique()
+handles, labels = scatter.legend_elements()
+ax.legend(handles, legend_labels, title='Cultivar')
+
 ax.set_xlabel("Plant number")
 ax.set_ylabel("Seed number")
 ax.set_zlabel("Seed weight")
 ax.set_title("Investigating the relationship between seed weight, seed number, and plant number on seed yield across three different cultivars")
-plt.show()
 
+plt.show()
